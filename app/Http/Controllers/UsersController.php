f0cee839e3c05a73d3e7ed183f7981599f04ea74
buding <1244685298@qq.com>
2019-09-02 23:51:00 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -53,4 +53,37 @@ class UsersController extends Controller
         session()->flash('success',"欢迎，您将在这里开启一段新的旅程~");
         return redirect()->route('users.show',[$user]);
     }
+
+    /**
+     * 显示个人信息编辑页面
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        //验证信息
+        $this->validate($request,[
+            'name'      => 'required|max:50',
+            'password'  => 'nullable|confirmed|min:6',
+        ]);
+
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        //更新信息
+        $user->update($data);
+
+        session()->flash('success',"个人资料更新成功！");
+
+        return redirect()->route('users.show',$user);
+
+
+    }
 }
