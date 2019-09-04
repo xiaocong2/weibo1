14c17d38d04ae3d1b95fa4c1ace0657714cca822
buding <1244685298@qq.com>
2019-09-05 00:48:06 +0800

    微博数据模型

---
+++ app/Models/Status.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 微博Model
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    /**
+     * 指定一条微博属于一个用户（1对1关系）
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,13 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    /**
+     * 指明一个用户有多条微博（1对多的关系）
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
