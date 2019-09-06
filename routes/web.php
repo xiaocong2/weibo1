6cc50b25b52dfd73b3352a48f25c18d0e7cc1bd5
buding <1244685298@qq.com>
2019-09-07 00:02:35 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/about','StaticPagesController@about')->name('about');//关于
 
 Route::get('signup', 'UsersController@create')->name('signup');//注册页面
 Route::resource('users','UsersController');//用户操作（增删改查）
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');//显示用户关注人列表
+Route::get('/users/{user}/followers','UsersController@followers')->name('users.followers');//显示用户粉丝列表
 
 Route::get('login', 'SessionsController@create')->name('login');//显示登录页
 Route::post('login', 'SessionsController@store')->name('login');//用户登录操作
